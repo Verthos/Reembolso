d3beac74ce5796ccd7f0782a77d93a203f44040d
William Kszan <wpkszan@gmail.com>
2022-05-17 22:41:28 -0300

    Adding authentication controller

---
+++ Reembolso/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Reembolso.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public AuthController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+
+        private string CreateToken(IEnumerable<Claim> claims, DateTime expiresAt)
+        {
+            var secretKey = Encoding.ASCII.GetBytes(_config.GetValue<string>("SecretKey"));
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: expiresAt,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] Credential credential)
+        {
+
+            //verify credential
+
+            if(credential.UserName == "admin" && credential.Password == "password")
+            {
+                //Creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@email.com.br"),
+                    new Claim("Department", "admin"),
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true")                    
+                };
+
+                var expiresAt = DateTime.Now.AddMinutes(10);
+                return Ok(new
+                {
+                    access_token = CreateToken(claims, expiresAt),
+                    expires_at = expiresAt,
+                });
+                
+            }
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint.");
+            return Unauthorized(ModelState);
+        }
+
+
+        public class Credential
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
